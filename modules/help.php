d83f93052dead75d29f8780f1e74228a944d3319
Lorenzzz <116018011+LorenzoZ-DEV@users.noreply.github.com>
2025-07-18 03:09:28 +0200

    Update help.php

--- modules/help.php
+++ modules/help.php
@@ -1,22 +1,28 @@
 <?php
+
 function help() {
-    logo();
+    if (function_exists('logo')) {
+        logo();
+    }
+
     echo <<<EOL
 \033[01;37m -----------------------------------------------
-|         \033[01;36mcommand\033[01;37m        |        \033[01;36mUse\033[01;37m           |
+|         \033[01;36mCommand\033[01;37m        |         \033[01;36mUse\033[01;37m          |
  -----------------------------------------------
-| \033[01;32mtrace -m\033[01;37m               | \033[01;33mTrack your IP\033[01;37m        |
-| \033[01;32mtrace -t <traget-ip>\033[01;37m   | \033[01;33mTrack IP\033[01;37m             |
-| \033[01;32mtrace --help\033[01;37m           | \033[01;33mFor more information\033[01;37m |
+| \033[01;32mtrace -m\033[01;37m              | \033[01;33mTrack your IP\033[01;37m        |
+| \033[01;32mtrace -t <target-ip>\033[01;37m | \033[01;33mTrack a specific IP\033[01;37m   |
+| \033[01;32mtrace --help\033[01;37m         | \033[01;33mShow this help menu\033[01;37m   |
  -----------------------------------------------
 
-\033[01;33m Note :- \033[01;31mip-api will automatically ban any IP addresses doing over 150 requests per minute.\033[00m
-
+\033[01;33m Note: \033[01;31mip-api will automatically ban any IP addresses doing over 150 requests per minute.\033[00m
 
 EOL;
-  $prompt="\033[00m";
-  echo $prompt;
-  $getact = readline(' IP-Tracer >> ');
-  menu();
+
+    echo "\033[00m";
+    
+    if (posix_isatty(STDIN)) {
+        $getact = readline(" IP-Tracer >> ");
+        menu(); 
+    }
 }
 ?>
