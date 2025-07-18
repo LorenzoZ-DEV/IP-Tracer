2d5ff221a1b61124dd7b1c9005e2c01de657ba94
Lorenzzz <116018011+LorenzoZ-DEV@users.noreply.github.com>
2025-07-18 03:13:43 +0200

    Update .traceip.php

--- modules/.traceip.php
+++ modules/.traceip.php
@@ -1,13 +1,29 @@
 <?php
 function trac($ip) {
+    if (empty($ip)) {
+        echo "\033[01;31mError: No IP address provided.\033[00m\n";
+        exit(1);
+    }
 
-$data = @unserialize(file_get_contents("http://ip-api.com/php/".$ip));
-$FCL="\033[01;33m";
-$MCL="\033[01;37m>\033[01;32m";
-$NCL="\033[00m";
-date_default_timezone_set($data['timezone']);
-system("clear");
-  echo <<<EOL
+    $url = "http://ip-api.com/php/" . urlencode($ip);
+    $data = @unserialize(file_get_contents($url));
+
+    $FCL = "\033[01;33m";
+    $MCL = "\033[01;37m>\033[01;32m";
+    $NCL = "\033[00m";
+
+    if (!$data || !is_array($data)) {
+        echo "\033[01;31m[!] Failed to fetch IP data. Check your internet connection or IP validity.\033[00m\n";
+        exit(1);
+    }
+
+    if (!empty($data['timezone'])) {
+        @date_default_timezone_set($data['timezone']);
+    }
+
+    system("clear");
+
+    echo <<<EOL
 \033[01;33m
 
 
@@ -17,7 +33,6 @@ system("clear");
      | |  __/  | |_| | | (_| | (_|  __/ |
      |_|_|      \__|_|  \__,_|\___\___|_|
 
-
    \033[01;37m}\033[01;31m----------------------------------------\033[01;37m{
 }\033[01;31m--------------- \033[01;32mIP Information\033[01;31m ---------------\033[01;37m{
    }\033[01;31m----------------------------------------\033[01;37m{
@@ -25,28 +40,33 @@ system("clear");
 \033[00m
 EOL;
 
-if(!empty($data['status']) && $data['status'] == 'success') {
-echo "\n ".$FCL."IP Address    ".$MCL."   ".$data['query'];
-echo "\n ".$FCL."Country code  ".$MCL."   ".$data['countryCode'];
-echo "\n ".$FCL."Country       ".$MCL."   ".$data['country'];
-echo "\n ".$FCL."Date & Time   ".$MCL."   ".date("F j, Y, g:i a");
-echo "\n ".$FCL."Region code   ".$MCL."   ".$data['region'];
-echo "\n ".$FCL."Region        ".$MCL."   ".$data['regionName'];
-echo "\n ".$FCL."City          ".$MCL."   ".$data['city'];
-echo "\n ".$FCL."Zip code      ".$MCL."   ".$data['zip'];
-echo "\n ".$FCL."Time zone     ".$MCL."   ".$data['timezone'];
-echo "\n ".$FCL."ISP           ".$MCL."   ".$data['isp'];
-echo "\n ".$FCL."Organization  ".$MCL."   ".$data['org'];
-echo "\n ".$FCL."ASN           ".$MCL."   ".$data['as'];
-echo "\n ".$FCL."Latitude      ".$MCL."   ".$data['lat'];
-echo "\n ".$FCL."Longtitude    ".$MCL."   ".$data['lon'];
-echo "\n ".$FCL."Location      ".$MCL."   ".$data['lat'].",".$data['lon'];
-echo "\n\n$NCL";
+    if ($data['status'] === 'success') {
+        echo "\n {$FCL}IP Address    {$MCL}   {$data['query']}";
+        echo "\n {$FCL}Country code  {$MCL}   {$data['countryCode']}";
+        echo "\n {$FCL}Country       {$MCL}   {$data['country']}";
+        echo "\n {$FCL}Date & Time   {$MCL}   " . date("F j, Y, g:i a");
+        echo "\n {$FCL}Region code   {$MCL}   {$data['region']}";
+        echo "\n {$FCL}Region        {$MCL}   {$data['regionName']}";
+        echo "\n {$FCL}City          {$MCL}   {$data['city']}";
+        echo "\n {$FCL}Zip code      {$MCL}   {$data['zip']}";
+        echo "\n {$FCL}Time zone     {$MCL}   {$data['timezone']}";
+        echo "\n {$FCL}ISP           {$MCL}   {$data['isp']}";
+        echo "\n {$FCL}Organization  {$MCL}   {$data['org']}";
+        echo "\n {$FCL}ASN           {$MCL}   {$data['as']}";
+        echo "\n {$FCL}Latitude      {$MCL}   {$data['lat']}";
+        echo "\n {$FCL}Longitude     {$MCL}   {$data['lon']}";
+        echo "\n {$FCL}Location      {$MCL}   {$data['lat']},{$data['lon']}\n\n{$NCL}";
+    } else {
+        echo "\n\033[01;31m Sorry unable to track the IP Address: {$ip} !!!\033[00m\n";
+        echo "\033[01;31m Check your \033[01;33mNetwork connection\033[01;31m or IP validity !!\033[00m\n\n";
+    }
+}
+
+// Check if IP argument is passed
+if (isset($argv[1])) {
+    trac($argv[1]);
 } else {
-  echo "\n\033[01;31m Sorry unable to track your\033[01;33m IP Address\033[01;31m !!!\033[00m\n";
-  echo "\033[01;31m Check your \033[01;33mNetwork connection\033[01;31m !!\033[00m\n";
-  echo "\033[01;31m If you are \033[01;33mOnline\033[01;31m then check your \033[01;33mIP Address\033[01;31m !!\033[00m\n\n";
+    echo "\033[01;31mUsage: php .traceip.php <target_ip>\033[00m\n";
+    exit(1);
 }
-}
-trac($argv[1]);
 ?>
